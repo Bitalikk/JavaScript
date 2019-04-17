717892b9173523e62bef010c6715b074ba197a93
Bitalikk <46748243+Bitalikk@users.noreply.github.com>
2019-04-17 11:39:38 +0300

    Update addClass.js

--- Module06/js/addClass.js
+++ Module06/js/addClass.js
@@ -194,92 +194,92 @@
 // level - рівень = 1
 // хр - досвід = 0
 // stamina - витривалість = 100
-class Hero {
-    constructor(name, life = 100, level = 1, xp = 0, stamina = 0) {
-        this._name = name;
-        this._life = life;
-        this._level = level;
-        this._xp = xp;
-        this._stamina = stamina;
-    }
+// class Hero {
+//     constructor(name, life = 100, level = 1, xp = 0, stamina = 0) {
+//         this._name = name;
+//         this._life = life;
+//         this._level = level;
+//         this._xp = xp;
+//         this._stamina = stamina;
+//     }
     // Клас буде мати методи :
     // 1) riseLevel - метод що збідьшує рівень героя на один, якщо хр = 1000
     // 2) addXp - метод що приймає параметром хр і плюсує поки сума небуде 1000. Коли хр стає тисячу метод обнуляє поле до 0.
     // 3) walk - метод що виводить в консоль фразу "name walking"
     // 4) rest - метод що відновлює поле stamina до початкового значення і виводить в консоль фразу 'Recovered' 
-    riseLevel() {
-        if(this._xp >= 1000) {
-            this._level +=1;
-            this.addXp(0);
-        }
-    }
-    addXp(xp) {
-        if(this._xp >= 1000) {
-            this._xp -= 1000;
-        }
-        this._xp += xp;
-    }
-    walk() {
-        console.log(`${this._name} walking`);
-    }
-    rest() {
-        this._stamina = 100;
-        console.log('Recovered');
-    }
-}
-// Створити героя і викликати для нього метод addXp(50) і addXp(950)
-// Далі викликати метод riseLevel() що збільшить рівень до 2.
-// викликати метод walk.
-const hero = new Hero('Wass', 100, 1, 0, 100);
-hero.addXp(50);
-hero.addXp(950);
-hero.riseLevel();
-hero.addXp(50); 
-hero.addXp(1000);
-hero.riseLevel();
-hero.addXp(1000);
-hero.riseLevel();
+//     riseLevel() {
+//         if(this._xp >= 1000) {
+//             this._level +=1;
+//             this.addXp(0);
+//         }
+//     }
+//     addXp(xp) {
+//         if(this._xp >= 1000) {
+//             this._xp -= 1000;
+//         }
+//         this._xp += xp;
+//     }
+//     walk() {
+//         console.log(`${this._name} walking`);
+//     }
+//     rest() {
+//         this._stamina = 100;
+//         console.log('Recovered');
+//     }
+// }
+// // Створити героя і викликати для нього метод addXp(50) і addXp(950)
+// // Далі викликати метод riseLevel() що збільшить рівень до 2.
+// // викликати метод walk.
+// const hero = new Hero('Wass', 100, 1, 0, 100);
+// hero.addXp(50);
+// hero.addXp(950);
+// hero.riseLevel();
+// hero.addXp(50); 
+// hero.addXp(1000);
+// hero.riseLevel();
+// hero.addXp(1000);
+// hero.riseLevel();
 
 
 
-console.log(hero);
-hero.walk();
+// console.log(hero);
+// hero.walk();
 
-// Part 2 
-// Написати класи Людина, Гном, Маг які будуть насладувати все від класа Герой.
-// Ці 3 класа будуть мати додаткову властивість race - раса.
-// Людина додатково матиме поле - strength = 100
-// Гном матиме додатково поле - rage = 100
-// Маг матиме додаткове поле - mana = 100
+// // Part 2 
+// // Написати класи Людина, Гном, Маг які будуть насладувати все від класа Герой.
+// // Ці 3 класа будуть мати додаткову властивість race - раса.
+// // Людина додатково матиме поле - strength = 100
+// // Гном матиме додатково поле - rage = 100
+// // Маг матиме додаткове поле - mana = 100
 
-// Для людини додати метод - wardenFight - який при кожному запуску зменьшує поле stamina на 20.
-// Коли поле stamina стане 0 потрібно викликати метод rest() щоб відновити її значення.
-function Human(name, life, level, xp, stamina) {
-  Hero.call(this, name, life, level, xp, stamina);
-  this.race = 'human';
-  this.strength = 100;
-}
-Human.prototype = Object.create(Hero.prototype);
-Human.prototype.wardenFight = function() {
-  this._stamnia -= 20;
-  if(this._stamnia === 0) this.rest();
-};
+// // Для людини додати метод - wardenFight - який при кожному запуску зменьшує поле stamina на 20.
+// // Коли поле stamina стане 0 потрібно викликати метод rest() щоб відновити її значення.
+// function Human(name, life, level, xp, stamina) {
+//   Hero.call(this, name, life, level, xp, stamina);
+//   this.race = 'human';
+//   this.strength = 100;
+// }
+// Human.prototype = Object.create(Hero.prototype);
+// Human.prototype.wardenFight = function() {
+//   this._stamnia -= 20;
+//   if(this._stamnia === 0) this.rest();
+// };
 
-// Для гнома додати метод berserk'sFury - який при кожному виклику збільшує поле 
-// rage на 30. Якщо rage = 100 обнулити поле stamina і rage та викликати метод rest().
+// // Для гнома додати метод berserk'sFury - який при кожному виклику збільшує поле 
+// // rage на 30. Якщо rage = 100 обнулити поле stamina і rage та викликати метод rest().
 
-function Draft(name, life, level, xp, stamina) {
-  Hero.call(this, name, life, level, xp, stamina);
-  this.race = 'draft';
-  this.rage = 100;
-} 
-Draft.prototype = Object.create(Hero.prototype);
-Draft.prototype = function() {
+// function Draft(name, life, level, xp, stamina) {
+//   Hero.call(this, name, life, level, xp, stamina);
+//   this.race = 'draft';
+//   this.rage = 100;
+// } 
+// Draft.prototype = Object.create(Hero.prototype);
+// Draft.prototype = function() {
 
-};
+// };
 
 
-// Для мага додати метод spell який при виклику зменьшує занчення на 25 і stamina на 25.
+// // Для мага додати метод spell який при виклику зменьшує занчення на 25 і stamina на 25.
 // Коли значення stamina стає 0 потрібно викликати метод rest(). 
 // Додати метод reload() який оновлює поле mana до 100
 
@@ -495,173 +495,173 @@ Draft.prototype = function() {
 /**
  * Класс, объекты которого описывают параметры гамбургера.
  */
-// class Hamburger {
-//     /**
-//      * @constructor
-//      * @param {String} size - Размер
-//      * @param {String} stuffing - Начинка
-//      */
-//     constructor(size, stuffing) {
-//       this._size = size;
-//       this._stuffing = stuffing;
-//       this._toppings = [];
-//     }
+class Hamburger {
+    /**
+     * @constructor
+     * @param {String} size - Размер
+     * @param {String} stuffing - Начинка
+     */
+    constructor(size, stuffing) {
+      this._size = size;
+      this._stuffing = stuffing;
+      this._toppings = [];
+    }
   
-//     /**
-//      * Добавить topping к гамбургеру. Можно добавить несколько topping, при условии, что они разные.
-//      * @param {String} topping - Тип добавки
-//      */
-//     addTopping(topping) {
-//         if(!this._toppings.includes(topping)) {
-//             this._toppings.push(topping);
-//         }
-//     }
+    /**
+     * Добавить topping к гамбургеру. Можно добавить несколько topping, при условии, что они разные.
+     * @param {String} topping - Тип добавки
+     */
+    addTopping(topping) {
+        if(!this._toppings.includes(topping)) {
+            this._toppings.push(topping);
+        }
+    }
         
-//     /**
-//      * Убрать topping, при условии, что она ранее была добавлена
-//      * @param {String} topping - Тип добавки
-//      */
-//     removeTopping(topping) {
-//         if(this._toppings.includes(topping)) {
-//             this._toppings.splice(this._toppings.indexOf(topping), 1);
-//         }
-//     }
+    /**
+     * Убрать topping, при условии, что она ранее была добавлена
+     * @param {String} topping - Тип добавки
+     */
+    removeTopping(topping) {
+        if(this._toppings.includes(topping)) {
+            this._toppings.splice(this._toppings.indexOf(topping), 1);
+        }
+    }
   
-//     /**
-//      * Получить список toppings
-//      * @returns {Array} - Массив добавленных topping, содержит значения констант Hamburger.TOPPING_*
-//      *
-//      * Попробуйте сделать это геттером чтобы можно было обращаться как obj.toppings и нам вернет массив добавок
-//      */
-//     get toppings() {
-//         return this._toppings;
-//     }
+    /**
+     * Получить список toppings
+     * @returns {Array} - Массив добавленных topping, содержит значения констант Hamburger.TOPPING_*
+     *
+     * Попробуйте сделать это геттером чтобы можно было обращаться как obj.toppings и нам вернет массив добавок
+     */
+    get toppings() {
+        return this._toppings;
+    }
   
-//     /**
-//      * Узнать размер гамбургера
-//      * @returns {String} - размер гамбургера
-//      *
-//      * Попробуйте сделать это геттером чтобы можно было обращаться как obj.size и нам вернет размер
-//      */
-//     get size() {
-//         return this._size;
-//     }
+    /**
+     * Узнать размер гамбургера
+     * @returns {String} - размер гамбургера
+     *
+     * Попробуйте сделать это геттером чтобы можно было обращаться как obj.size и нам вернет размер
+     */
+    get size() {
+        return this._size;
+    }
   
-//     /**
-//      * Узнать начинку гамбургера
-//      * @returns {String} - начинка гамбургера
-//      *
-//      * Попробуйте сделать это геттером чтобы можно было обращаться как obj.stuffing и нам вернет начинку
-//      */
-//     get stuffing() {
-//         return this._stuffing;
-//     }
+    /**
+     * Узнать начинку гамбургера
+     * @returns {String} - начинка гамбургера
+     *
+     * Попробуйте сделать это геттером чтобы можно было обращаться как obj.stuffing и нам вернет начинку
+     */
+    get stuffing() {
+        return this._stuffing;
+    }
   
-//     /**
-//      * Узнать цену гамбургера
-//      * @returns {Number} - Цена в деньгах
-//      *
-//      * Попробуйте сделать это геттером чтобы можно было обращаться как obj.price и нам вернет сумму.
-//      */
-//     get calculatePrice() {
-//         let price = Hamburger.SIZES[this._size].price + Hamburger.STUFFINGS[this._stuffing].price;
-//         if(this._toppings.length > 0) {
-//             price += this._toppings.reduce((acc, topping) => acc + Hamburger.TOPPINGS[topping].price, 0);
-//         }
-//         return price;
-//     }
+    /**
+     * Узнать цену гамбургера
+     * @returns {Number} - Цена в деньгах
+     *
+     * Попробуйте сделать это геттером чтобы можно было обращаться как obj.price и нам вернет сумму.
+     */
+    get calculatePrice() {
+        let price = Hamburger.SIZES[this._size].price + Hamburger.STUFFINGS[this._stuffing].price;
+        if(this._toppings.length > 0) {
+            price += this._toppings.reduce((acc, topping) => acc + Hamburger.TOPPINGS[topping].price, 0);
+        }
+        return price;
+    }
   
-//     /**
-//      * Узнать калорийность
-//      * @returns {Number} - Калорийность в калориях
-//      *
-//      * Попробуйте сделать это геттером чтобы можно было обращаться как obj.calories и нам вернет сумму.
-//      */
-//     get calculateCalories() {
-//         let cal = Hamburger.SIZES[this._size].calories + Hamburger.STUFFINGS[this._stuffing].calories;
-//         if(this._toppings.length > 0) {
-//             cal += this._toppings.reduce((acc, topping) => acc + Hamburger.TOPPINGS[topping].calories, 0);
-//         }
-//         return cal;
-//     }
-//   }
+    /**
+     * Узнать калорийность
+     * @returns {Number} - Калорийность в калориях
+     *
+     * Попробуйте сделать это геттером чтобы можно было обращаться как obj.calories и нам вернет сумму.
+     */
+    get calculateCalories() {
+        let cal = Hamburger.SIZES[this._size].calories + Hamburger.STUFFINGS[this._stuffing].calories;
+        if(this._toppings.length > 0) {
+            cal += this._toppings.reduce((acc, topping) => acc + Hamburger.TOPPINGS[topping].calories, 0);
+        }
+        return cal;
+    }
+  }
   
-//   /*
-//     Размеры, виды добавок и начинок объявите как статические поля класса.
-//     Добавьте отсутсвующие поля по аналогии с примером.
-//   */
-//   Hamburger.SIZE_SMALL = 'SIZE_SMALL';
-//   Hamburger.SIZE_LARGE = 'SIZE_LARGE';
+  /*
+    Размеры, виды добавок и начинок объявите как статические поля класса.
+    Добавьте отсутсвующие поля по аналогии с примером.
+  */
+  Hamburger.SIZE_SMALL = 'SIZE_SMALL';
+  Hamburger.SIZE_LARGE = 'SIZE_LARGE';
   
-//   Hamburger.SIZES = {
-//     [Hamburger.SIZE_SMALL]: {
-//       price: 30,
-//       calories: 50,
-//     },
-//     [Hamburger.SIZE_LARGE]: {
-//         price: 50,
-//         calories: 100,
-//     },
-//   };
+  Hamburger.SIZES = {
+    [Hamburger.SIZE_SMALL]: {
+      price: 30,
+      calories: 50,
+    },
+    [Hamburger.SIZE_LARGE]: {
+        price: 50,
+        calories: 100,
+    },
+  };
   
-//   Hamburger.STUFFING_CHEESE = 'STUFFING_CHEESE';
-//   Hamburger.STUFFING_SALAD = 'STUFFING_SALAD';
-//   Hamburger.STUFFING_MEAT = 'STUFFING_MEAT';
+  Hamburger.STUFFING_CHEESE = 'STUFFING_CHEESE';
+  Hamburger.STUFFING_SALAD = 'STUFFING_SALAD';
+  Hamburger.STUFFING_MEAT = 'STUFFING_MEAT';
   
-//   Hamburger.STUFFINGS = {
-//     [Hamburger.STUFFING_CHEESE]: {
-//       price: 15,
-//       calories: 20,
-//     },
-//     [Hamburger.STUFFING_SALAD]: {
-//         price: 20,
-//         calories: 5,
-//     },
-//     [Hamburger.STUFFING_MEAT]: {
-//         price: 35,
-//         calories: 15,
-//     },
-//   };
+  Hamburger.STUFFINGS = {
+    [Hamburger.STUFFING_CHEESE]: {
+      price: 15,
+      calories: 20,
+    },
+    [Hamburger.STUFFING_SALAD]: {
+        price: 20,
+        calories: 5,
+    },
+    [Hamburger.STUFFING_MEAT]: {
+        price: 35,
+        calories: 15,
+    },
+  };
   
-//   Hamburger.TOPPING_SPICE = 'TOPPING_SPICE';
-//   Hamburger.TOPPING_SAUCE = 'TOPPING_SAUCE';
+  Hamburger.TOPPING_SPICE = 'TOPPING_SPICE';
+  Hamburger.TOPPING_SAUCE = 'TOPPING_SAUCE';
   
-//   Hamburger.TOPPINGS = {
-//     [Hamburger.TOPPING_SPICE]: {
-//       price: 10,
-//       calories: 0,
-//     },
-//     [Hamburger.TOPPING_SAUCE]: {
-//         price: 15,
-//         calories: 5,
-//     },
-//   };
+  Hamburger.TOPPINGS = {
+    [Hamburger.TOPPING_SPICE]: {
+      price: 10,
+      calories: 0,
+    },
+    [Hamburger.TOPPING_SAUCE]: {
+        price: 15,
+        calories: 5,
+    },
+  };
   
-//   /* Вот как может выглядеть использование этого класса */
+  /* Вот как может выглядеть использование этого класса */
   
-//   // Маленький гамбургер с начинкой из сыра
-//   const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+  // Маленький гамбургер с начинкой из сыра
+  const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
   
-//   // Добавка из приправы
-//   hamburger.addTopping(Hamburger.TOPPING_SPICE);
+  // Добавка из приправы
+  hamburger.addTopping(Hamburger.TOPPING_SPICE);
   
-//   // Спросим сколько там калорий
-//   console.log("Calories: ", hamburger.calculateCalories);
+  // Спросим сколько там калорий
+  console.log("Calories: ", hamburger.calculateCalories);
   
-//   // Сколько стоит?
-//   console.log("Price: ", hamburger.calculatePrice);
+  // Сколько стоит?
+  console.log("Price: ", hamburger.calculatePrice);
   
-//   // Я тут передумал и решил добавить еще соус
-//   hamburger.addTopping(Hamburger.TOPPING_SAUCE);
+  // Я тут передумал и решил добавить еще соус
+  hamburger.addTopping(Hamburger.TOPPING_SAUCE);
   
-//   // А сколько теперь стоит?
-//   console.log("Price with sauce: ", hamburger.calculatePrice);
+  // А сколько теперь стоит?
+  console.log("Price with sauce: ", hamburger.calculatePrice);
   
-//   // Проверить, большой ли гамбургер?
-//   console.log("Is hamburger large: ", hamburger.size === Hamburger.SIZE_LARGE); // -> false
+  // Проверить, большой ли гамбургер?
+  console.log("Is hamburger large: ", hamburger.size === Hamburger.SIZE_LARGE); // -> false
   
-//   // Убрать добавку
-//   hamburger.removeTopping(Hamburger.TOPPING_SPICE);
+  // Убрать добавку
+  hamburger.removeTopping(Hamburger.TOPPING_SPICE);
   
-//   // Смотрим сколько добавок
-//   console.log("Hamburger has %d toppings", hamburger.toppings.length); // 1
+  // Смотрим сколько добавок
+  console.log("Hamburger has %d toppings", hamburger.toppings.length); // 1
